e103349b1c2179ae3a42b1ad67f9cd8d7bb1961d
PUNJAGONS <punjagons@gmail.com>
2024-08-02 15:42:33 +0700

    init file to start learning modules
    (cherry picked from commit 218e58c0aa84442dfd3452c7d2299155eff24522)

---
+++ src/bin/organization/module.rs
@@ -0,0 +1,28 @@
+// --------------------------------------------
+//          Code Organization
+// --------------------------------------------
+
+/*  
+    Packages
+    - Managed through Cargo commands
+    - Highest level of code organization
+    - Contains Crates
+
+    Crates
+    - A compilation unit
+    - Can be a binary crate or library crate
+    - Contains Modules
+
+    Modules
+    - Controls at a finer level, the structure, visibility and privacy
+
+    ** Rules for package **
+    - Must have at least one crate
+    - At most 1 library crate
+    - Any number of binary crates
+ */
+
+
+fn main() {
+
+}
