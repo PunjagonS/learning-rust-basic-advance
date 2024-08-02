f8b8ba0b61f27e83f0ca014c11069f1e89a35cb0
PUNJAGONS <punjagons@gmail.com>
2024-08-02 11:55:53 +0700

    add more description for enum
    (cherry picked from commit c4111a310061dfc78909b1c26a130255f630614d)

--- src/bin/types/enums.rs
+++ src/bin/types/enums.rs
@@ -2,6 +2,50 @@
 //          Enums
 // --------------------------------------------
 
+/*
+    An enumeration, also referred to as an enum, is a simultaneous definition of a nominal enumerated type as well as a set of constructors,
+    that can be used to create or pattern-match values of the corresponding enumerated type.
+
+    Enumerations are declared with the keyword enum.
+
+    Enumeration constructors can have Tuple, Struct, Unit either named or unnamed fields:
+
+    In this example, Cat is a struct-like enum variant, whereas Dog is simply called an enum variant 
+    where Dog is Tuple and Cat is Struct
+    enum Animal {
+        Dog(String, f64),                       unnamed field (String, f64)
+        Cat{ name: String, weight: f64 },       named
+    }
+
+    An enum where no constructors contain fields are called a field-less enum. For example, this is a fieldless enum:
+    enum Fieldless {
+        Tuple(),            - enum variant
+        Struct{},           - struct-like enum variant
+        Unit,               - unit variants
+    }
+
+    Structs and enums are powerful constructs on their own, but they can be even more powerful when combined. 
+    By using enums to group related structs or adding a field to a struct that is an enum type, 
+    you can create complex data types that accurately model your problem domain.
+
+    enum Enemy {
+        Goomba(Goomba),
+        KoopaTroopa(KoopaTroopa),
+        HammerBro(HammerBro),
+    }
+
+    struct Goomba {
+        position: Position,
+        is_stomped: bool,
+    }
+
+    struct KoopaTroopa {
+        position: Position,
+        is_stomped: bool,
+        is_in_shell: bool,
+    }
+ */
+
 #[derive(Debug)]
 enum WeekDay {
     Monday,
@@ -51,6 +95,8 @@ fn main() {
     let mut day = "Saturday".to_string();
     println!("Day: {day}");
 
+    // Not effective way before enum exist
+    // Cost of maintenance, flexible, easy re-use
     let week_day = vec![
         "Monday".to_string(),
         "Tuesday".to_string(),
