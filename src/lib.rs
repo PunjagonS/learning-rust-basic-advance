cfaeb16f49b17042fa2ebe107506815ba2ccd298
PUNJAGONS <punjagons@gmail.com>
2024-08-09 15:09:02 +0700

    fix operation logic return of fn total_bill

--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,7 @@ mod order {
         fn total_bill(&self) -> f64 {
             let discount = self.calculate_discount();
             let total_before_discount = self.product.product_price() * self.quantity as f64;
-            total_before_discount = (total_before_discount * discount)
+            total_before_discount - (total_before_discount * discount)
         }
     }
 }
