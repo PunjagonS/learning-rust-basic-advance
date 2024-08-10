39b71b02b7625d084a9d90db3bbc83f3a49be34c
PUNJAGONS <punjagons@gmail.com>
2024-08-10 16:37:55 +0700

    show more marcro using.

--- src/modules/testing.rs
+++ src/modules/testing.rs
@@ -10,7 +10,11 @@ mod tests {
     fn larger_circle_should_contain_smaller(){
         let larger_circle = Circle::new(5.0);
         let smaller_circle = Circle::new(2.0);
-        assert_eq!(larger_circle.contains(&smaller_circle), true);
+        assert_eq!(larger_circle.contains(&smaller_circle), true);                                      // Macro equal
+        // assert_eq!(larger_circle.contains(&smaller_circle), true, "Custom failure message");         // Add custom failure message
+
+        assert_ne!(larger_circle.contains(&smaller_circle), false);                                     // Macro not equal
+        assert!(larger_circle.contains(&smaller_circle));                                               // Macro default boolean "true"
     }
 
     #[test]
