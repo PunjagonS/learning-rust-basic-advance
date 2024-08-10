b761aa92066a04354be78512b4ef62d8777c04fc
PUNJAGONS <punjagons@gmail.com>
2024-08-10 12:33:59 +0700

    import modules into bin crate

--- src/bin/organization/module.rs
+++ src/bin/organization/module.rs
@@ -22,7 +22,19 @@
     - Any number of binary crates
  */
 
+use basic_advance::modules::{Customer, Product, Category, Order};
 
 fn main() {
+    // let product = Product{
+    //     id: 1,
+    //     name: String::from("Laptop"),
+    //     price: 799.99,
+    //     category: Category::Electronics,
+    // };
 
+    let product = Product::new(1, "Laptop".to_string(), 799.99, Category::Electronics);
+    let customer = Customer::new(1, "Alice". to_string(), "alice@example.com".to_string());
+    let order = Order::new(1, product, customer, 2);
+
+    println!("Total cost is: &{}", order.total_bill());
 }
