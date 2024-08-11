bb56f90158d8b52b5023658305e0bcc140610eac
PUNJAGONS <punjagons@gmail.com>
2024-08-11 15:07:56 +0700

    add more use test case using result lib standard instead of assert.

--- src/modules/shapes.rs
+++ src/modules/shapes.rs
@@ -7,6 +7,14 @@ impl Circle{
         Circle{radius}
     }
 
+    pub fn new_1(radius: f32) -> Result<Circle, String> {
+        if radius >= 0.0 {
+            Ok(Circle {radius})
+        }else {
+            Err("radius should be positive.".to_string())
+        }
+    }
+
     pub fn contains(&self, other: &Circle) -> bool {
         self.radius > other.radius
     }
--- src/modules/testing.rs
+++ src/modules/testing.rs
@@ -4,17 +4,17 @@
 
 #[cfg(test)]
 mod tests {
-    use super::super::*;                                                                               // Import all parent modules
+    use super::super::*;                                                                         // Import all parent modules
 
     #[test]
     fn larger_circle_should_contain_smaller(){
         let larger_circle = Circle::new(5.0);
         let smaller_circle = Circle::new(2.0);
-        // assert_eq!(larger_circle.contains(&smaller_circle), true);                                   // Macro equal
-        assert_eq!(larger_circle.contains(&smaller_circle), true, "Custom failure message");            // Add custom failure message
+        // assert_eq!(larger_circle.contains(&smaller_circle), true);                             // Macro equal
+        assert_eq!(larger_circle.contains(&smaller_circle), true, "Custom failure message");      // Add custom failure message
 
-        assert_ne!(larger_circle.contains(&smaller_circle), false);                                     // Macro not equal
-        assert!(larger_circle.contains(&smaller_circle));                                               // Macro default boolean "true"
+        assert_ne!(larger_circle.contains(&smaller_circle), false);                               // Macro not equal
+        assert!(larger_circle.contains(&smaller_circle));                                         // Macro default boolean "true"
     }
 
     #[test]
@@ -22,5 +22,16 @@ mod tests {
         let larger_circle = Circle::new(5.0);
         let smaller_circle = Circle::new(2.0);
         assert_eq!(!smaller_circle.contains(&larger_circle), true);
+    }
+
+    /*  Example of using standard lib Result 
+        to define this test case failed or pass
+        instead of used assert directly.
+     */
+    #[test]
+    fn should_not_create_circle() -> Result<(), String> {
+        Circle::new_1(0.01)?;                                                               // "?" shorthand to return Err if new_1() is error
+        // Circle::new_1(-0.1)?;                                                                                                            
+        Ok(())
     }
 }
