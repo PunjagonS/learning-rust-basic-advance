85d20738b52a8a821257ccf5d08cc19d2bda3e60
PUNJAGONS <punjagons@gmail.com>
2024-08-11 14:48:13 +0700

    rearrange comment space and uncomment assert macro with custom error message

--- src/modules/testing.rs
+++ src/modules/testing.rs
@@ -4,14 +4,14 @@
 
 #[cfg(test)]
 mod tests {
-    use super::super::*;                                               // Import all parent modules
+    use super::super::*;                                                                               // Import all parent modules
 
     #[test]
     fn larger_circle_should_contain_smaller(){
         let larger_circle = Circle::new(5.0);
         let smaller_circle = Circle::new(2.0);
-        assert_eq!(larger_circle.contains(&smaller_circle), true);                                      // Macro equal
-        // assert_eq!(larger_circle.contains(&smaller_circle), true, "Custom failure message");         // Add custom failure message
+        // assert_eq!(larger_circle.contains(&smaller_circle), true);                                   // Macro equal
+        assert_eq!(larger_circle.contains(&smaller_circle), true, "Custom failure message");            // Add custom failure message
 
         assert_ne!(larger_circle.contains(&smaller_circle), false);                                     // Macro not equal
         assert!(larger_circle.contains(&smaller_circle));                                               // Macro default boolean "true"
