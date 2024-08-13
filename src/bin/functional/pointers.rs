31fe8209650735e41978fd09038a38cf6b7192c1
PUNJAGONS <punjagons@gmail.com>
2024-08-13 13:35:12 +0700

    complete convert closures to function pointers.

--- src/bin/functional/pointers.rs
+++ src/bin/functional/pointers.rs
@@ -15,16 +15,31 @@ struct User {
     salary: u32,
 }
 
-fn is_valid_user<V1, V2>(name: &str, age: u8, simple_validator: V1, advance_validator: V2) -> bool 
-where
-    V1: Fn(&str) -> bool,
-    V2: Fn(u8) -> bool,
+//// Convert to function pointers concrete type
+// fn is_valid_user<V1, V2>(name: &str, age: u8, simple_validator: V1, advance_validator: V2) -> bool 
+// where
+//     V1: Fn(&str) -> bool,
+//     V2: Fn(u8) -> bool,
+// {
+//     simple_validator(name) && advance_validator(age)
+// }
+
+fn is_valid_user(
+    name: &str,
+    banned_user: &str,
+    age: u8, 
+    simple_validator: fn(&str, &str) -> bool, 
+    advance_validator: fn(u8) -> bool ) -> bool
 {
-    simple_validator(name) && advance_validator(age)
+    simple_validator(name, banned_user) && advance_validator(age)
 }
 
-fn validate_user_simple(name: &str) -> bool {
-    name.len() != 0
+fn validate_user_simple(name: &str, banned_user: &str) -> bool {
+    name.len() != 0 && name != banned_user
+}
+
+fn validate_user_advance(age: u8) -> bool {
+    age >= 30
 }
 
 fn main() {
@@ -33,12 +48,15 @@ fn main() {
         age: 35,
         salary: 40_000,
     };
+    let banned_user = "banned_user";
 
+    // Replace closeures with function pointers 
     // let validate_user_simple = |name: &str| name.len() != 0;
-    let validate_user_advance = |age: u8| age >= 30;
+    // let validate_user_advance = |age: u8| age >= 30;
     
     println!("User valididy {}", is_valid_user(
         &person_1.name,
+        &banned_user,
         person_1.age,
         validate_user_simple,
         validate_user_advance
