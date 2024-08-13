a8bf45761d46cae8151c6e302bf09e18c95d3e11
PUNJAGONS <punjagons@gmail.com>
2024-08-13 13:18:01 +0700

    init file for learning function pointers

---
+++ src/bin/functional/pointers.rs
@@ -0,0 +1,42 @@
+// --------------------------------------------
+//          Function Pointers
+// --------------------------------------------
+
+/*
+    Function Pointers are concrete type, it works similar to closures, 
+    except that they don't capture the variables in their environment.
+
+    "fn(..) -> .." is the signature of the Function Pointers.
+*/
+
+struct User {
+    name: String,
+    age: u8,
+    salary: u32,
+}
+
+fn is_valid_user<V1, V2>(name: &str, age: u8, simple_validator: V1, advance_validator: V2) -> bool 
+where
+    V1: Fn(&str) -> bool,
+    V2: Fn(u8) -> bool,
+{
+    simple_validator(name) && advance_validator(age)
+}
+
+fn main() {
+    let person_1 = User {
+        name: "Someone".to_string(),
+        age: 35,
+        salary: 40_000,
+    };
+
+    let validate_user_simple = |name: &str| name.len() != 0;
+    let validate_user_advance = |age: u8| age >= 30;
+    
+    println!("User valididy {}", is_valid_user(
+        &person_1.name,
+        person_1.age,
+        validate_user_simple,
+        validate_user_advance
+    ));
+}
