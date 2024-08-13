9ac62fa4ceef0d5f5f884f2aa403c47f515f4c38
PUNJAGONS <punjagons@gmail.com>
2024-08-13 10:27:56 +0700

    refactor output associated types to generics support line struct

--- src/bin/abstraction/associated_types.rs
+++ src/bin/abstraction/associated_types.rs
@@ -81,9 +81,8 @@ impl DistanceThreeHours for Mph {
     to allow us implement multiple times.
 */
 
-trait Addition<T> {
-    type Output;
-    fn add(self, rhs: T) -> Self::Output;
+trait Addition<T, U> {
+    fn add(self, rhs: T) -> U;
 }
 
 struct Point {
@@ -91,9 +90,14 @@ struct Point {
     y: i32,
 }
 
-impl Addition<Point> for Point {
-    type Output = Point;
-    fn add(self, rhs: Point) -> Self::Output {
+struct Line {
+    start: Point,
+    end: Point,
+}
+
+// First implement trait
+impl Addition<Point, Point> for Point {
+    fn add(self, rhs: Point) -> Point {
         Point {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
@@ -101,12 +105,22 @@ impl Addition<Point> for Point {
     }
 }
 
-impl Addition<i32> for Point {
-    type Output = Point;
-    fn add(self, rhs: i32) -> Self::Output {
+// Second implement trait
+impl Addition<i32, Point> for Point {
+    fn add(self, rhs: i32) -> Point {
         Point {
             x: self.x + rhs,
             y: self.y + rhs,
+        }
+    }
+}
+
+// Third implement trait
+impl Addition<Point, Line> for Point {
+    fn add(self, rhs: Point) -> Line {
+        Line {
+            start: self,
+            end: rhs,
         }
     }
 }
@@ -121,4 +135,30 @@ fn main() {
     let distance_miles = speed_mph.distance_in_three_hours();
 
     println!("At {:?}, you will travel {:?} in 3 hours.", speed_mph, distance_miles);
+
+
+    /////////////////////////////////////////////////////////////////////////////////
+    let p1 = Point { x:1, y:1 };
+    let p2 = Point { x:2, y:2 };
+
+    /*
+        Need to giving an explicit type for p3 because self.add(Point) is matched 2 implements 
+        at 1 and 3 as 1 return Point and 3 return line so possible output for p3 is Point or Line
+    */
+    let p3: Point = p1.add(p2);
+
+    assert_eq!(p3.x, 3);
+    assert_eq!(p3.y, 3);
+
+    let p1 = Point { x:1, y:1 };
+    let p3 = p1.add(2);
+
+    assert_eq!(p3.x, 3);
+    assert_eq!(p3.y, 3);
+
+    let p1 = Point { x:1, y:1 };
+    let p2 = Point { x:2, y:2 };
+    let line: Line = p1.add(p2);
+
+    assert!(line.start.x == 1 && line.start.y == 1 && line.end.x == 2 && line.end.y == 2 );
 }
