383e7c6a25e05451c0394977e6d923de04a3bfbe
PUNJAGONS <punjagons@gmail.com>
2024-08-16 00:06:31 +0700

    completed box smart pointer part2.

--- src/bin/memory/box_smart_pointer.rs
+++ src/bin/memory/box_smart_pointer.rs
@@ -43,12 +43,49 @@ enum Conveyance {
     Nil,
  }
 
+ #[derive(Debug)]
+ enum ListImproved {
+    Cons(i32, Option<Box<ListImproved>>),
+ }
+
+ trait Storage {}
+ struct HugeData;
+ struct SmallData;
+
+ impl Storage for HugeData {}
+ impl Storage for SmallData {}
+
 fn main() {
     let x = 0.625;             
-    let y = Box::new(x);                                                // Pointing to some heap memory containing the value.
-    let z = &x;                                                             // Pointing to some memory location on the stack.
+    let y = Box::new(x);                                    // Pointing to some heap memory containing the value.
+    let z = &x;                                                 // Pointing to some memory location on the stack.
 
-    let list = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))));
+    let list = List::Cons(1, Box::new(List::Cons(2, 
+        Box::new(List::Cons(3, 
+            Box::new(List::Nil))))));
+    println!("list : {:?}", list);
 
-    println!("{:?}", list);
+    let list_improved = ListImproved::Cons(1, 
+        Some(Box::new(ListImproved::Cons(2, 
+            Some(Box::new(ListImproved::Cons(3, None)))))));
+    println!("list_improved : {:?}", list_improved);
+
+
+    /*
+        Box is also useful when copying large amounts of data 
+        when transfer ownership.
+    */
+    let data_1 = HugeData;
+    let data_2 = Box::new(HugeData);
+
+    let data_3 = data_1;                                    // data_1 is copied entire around the stack transfer to data_3.
+    let data_4 = data_2;                               // data_2, only the box pointer will be copied(small amount of data).
+
+
+    let data_5 = Box::new(SmallData);
+    /*
+        Use "trait objects" to let vector store difference types of data
+        implemented from Storage trait by set the type annotation.
+    */
+    let data: Vec<Box<dyn Storage>> = vec![Box::new(data_3), data_4, data_5];
 }
