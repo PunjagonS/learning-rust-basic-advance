a265f638a20ad24b74ecbeddd5448570f25fd4dc
PUNJAGONS <punjagons@gmail.com>
2024-08-18 14:33:03 +0700

    completed how to init struct follow rust convention.

--- src/bin/design_patterns/init_struct.rs
+++ src/bin/design_patterns/init_struct.rs
@@ -2,6 +2,29 @@
 //          Inittializing Struct Instance
 // --------------------------------------------
 
-fn main() {
+use basic_advance::modules::Student;
 
+fn main() {
+    // let std_1 = Student {
+    //     id: 11,
+    //     age: 20,
+    //     name: "Joseph".to_string(),
+    // };
+
+    let std_1 = Student::new("Joseph".to_string());
+    println!("{:?}", std_1);
+
+    let std_2 = Student::new("Joseph".to_string()).unwrap_or_default();
+    println!("{:?}", std_2);
+
+    let std_3 = Student::new("joseph123".to_string()).unwrap_or_default();
+    println!("{:?}", std_3);
+
+    let std_4 = Student::new("joseph".to_string()).unwrap_or_default();
+    println!("{:?}", std_4);
+
+    let std_5 = Student{
+        age: 12,
+        ..Default::default()
+    };
 }
--- src/modules/student.rs
+++ src/modules/student.rs
@@ -1,6 +1,32 @@
-#[derive(Debug)]
+// #[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Student {
-    id: u8,
+    pub id: u8,
     pub age: u8,
     pub name: String,
 }
+
+impl Student {
+    pub fn new(std_name: String) -> Result<Self, String> {
+        if std_name.chars().all(|x| matches!(x, 'a'..= 'z')) {
+            Ok(Self {
+                id: 0,
+                age: 20,
+                name: std_name,
+            })
+        } else {
+            Err("The name is invalid".to_string())
+        }
+    }
+}
+
+// Comment to use derive macro 'Default' instead
+// impl Default for Student {
+//     fn default() -> Self {
+//         Self {
+//             id: 0,
+//             age: 20,
+//             name: "Unknown".to_string(),
+//         }
+//     }
+// }
