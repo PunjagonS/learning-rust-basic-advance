7d1292ef5f8e7368e5e8a9a311d869990d338a18
PUNJAGONS <punjagons@gmail.com>
2024-08-20 15:11:18 +0700

    completed thread basics.

---
+++ src/bin/concurrency/thread_basics.rs
@@ -0,0 +1,33 @@
+// --------------------------------------------
+//              Thread Basics
+// --------------------------------------------
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    println!("This will be printed.");
+    println!("This will also be printed.");
+    println!("The concurrency will start after this line.");
+
+    // thread::spawn(|| {
+    let t = thread::spawn(|| {
+        println!("Hello 1 from the thread");
+        println!("Hello 2 from the thread");
+        println!("Hello 3 from the thread");
+        println!("Hello 4 from the thread");
+        println!("Hello 5 from the thread");
+        println!("Hello 6 from the thread");
+        println!("Hello 7 from the thread");
+    });
+
+    // Using sleep() to block main thread and let other threads work(not guaranteed!)
+    // thread::sleep(Duration::from_millis(1));
+
+    
+    // t.join();                            // Using join() to block main thread (guaranteed!)
+    println!("Hello 1 from the main");
+    println!("Hello 2 from the main");
+    println!("Hello 3 from the main");
+    t.join();                               // Using join() to block main thread (guaranteed!)
+}
