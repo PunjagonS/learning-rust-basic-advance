786e8979681ba6e160ab26368fc796b15eba8fcf
PUNJAGONS <punjagons@gmail.com>
2024-08-23 12:08:25 +0700

    fix error never loop from clippy lint.

--- src/bin/quickstart/controlflow.rs
+++ src/bin/quickstart/controlflow.rs
@@ -5,24 +5,24 @@
 // --------------------------------------------
 
 fn main() {
-    loop {
-        println!("Simple loop");
-        break;
-    }
+    // loop {
+    //     println!("Simple loop");
+    //     break;
+    // }
 
-    // Named loop for breaking outer loop
-    'outer: loop {
-        'inner: loop {
-            println!("Simple nested loop");
-            //break 'inner; // break inner loop but continue outer loop
-            break 'outer;
-        }
-    }
+    // // Named loop for breaking outer loop
+    // 'outer: loop {
+    //     'inner: loop {
+    //         println!("Simple nested loop");
+    //         //break 'inner; // break inner loop but continue outer loop
+    //         break 'outer;
+    //     }
+    // }
 
-    // Loop expression with return value 10 but not recommended and practical use
-    let a = loop {
-        break 10;
-    };
+    // // Loop expression with return value 10 but not recommended and practical use
+    // let a = loop {
+    //     break 10;
+    // };
 
     let vec = vec![1, 2, 3, 4, 5];
 
