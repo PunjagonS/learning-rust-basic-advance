71ea1b1e8acb7555580f3d352179f5b529a78ef3
PUNJAGONS <punjagons@gmail.com>
2024-08-23 11:35:43 +0700

    fix infinite loop bug.

--- src/bin/concurrency/message_channels_p2.rs
+++ src/bin/concurrency/message_channels_p2.rs
@@ -12,7 +12,7 @@ fn main() {
     thread::spawn(move || {
         let x = "some_value".to_string();
         println!("Sending value {x}");
-        thread::sleep(Duration::from_secs(3));      // Make rx blocked.
+        thread::sleep(Duration::from_secs(3)); // Make rx blocked.
         tx.send(x).unwrap();
     });
     // let recieved_val = rx.recv().unwrap();      // Be blocked from waiting value.
@@ -24,7 +24,8 @@ fn main() {
         match rx.try_recv() {
             Ok(recieved_value) => {
                 println!("Recieved value is: {recieved_value}");
-            },
+                recieved_status = true;
+            }
             Err(_) => println!("I am doing some other stuff."),
         }
     }
