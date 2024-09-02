287f9bd71155c77e137153227a6964c7b49aaf2b
PUNJAGONS <punjagons@gmail.com>
2024-09-02 14:55:06 +0700

    back to learn never type.

--- src/bin/size/zero_sized_never.rs
+++ src/bin/size/zero_sized_never.rs
@@ -1,21 +1,72 @@
 // --------------------------------------------
-//          Zero Sized Types (Never)
+//       Zero Sized Types (`!` Never Type)
 // --------------------------------------------
 
 /*
-    Never type represents computation that 
-    never resolves to any value. In others words,
-    it corressponds to computation that will always
-    panic or exit the program.
+    `!` is never type, it represents computation that
+    never resolves to any value.
 
-    The first use case of the never type is when we
-    want to signify that the function will not return normally.
+    Functions returning `!` are called diverging functions.
+    They can only panic, exit the program or result in
+    an infinite loop.
+
+    `!` nevver types means no value, and no value cannot be assigned
+    to something which needs a value.
 */
 
-// Indicate the compiler we want to use never type.
-// Not support on stable version
-// #![feature(never_type)]
+#![feature(never_type)] // Indicate the compiler we want to use never type (not support on stable version).
+fn unrecoverable_state() -> ! {
+    panic!("This function will not return normally with something valid");
+}
+
+/*
+    Another use case of the never type is that it can be used
+    to permit us to designate particular states as unreachable
+    at the type level.
+*/
+// fn function() -> Result<i32, String> {} // Normal form return string if error.
+// fn function_1() -> Result<i32, !> {} // This function promises to be never error. (if error happen program will panic or exit)
+// fn function_2() -> Result<!, i32> {} // This function promises to be never success. (if success program will panic or exit)
+
+// Own custom never type.
+enum NeverType {}
 
 fn main() {
-    
+    /*
+        The first use case of the never type is when we
+        want to signify that the function will not return normally.
+    */
+
+    // unrecoverable_state();
+    // let x= !; // Cannot directly assign `!` to variable.
+    // let x: ! = unrecoverable_state(); // This will not compile time error.
+    // let x: !; // Can create a variable of never type.
+    let x = panic!();
+
+    /*
+        `!` never types can be used in match arms that are guaranteed
+        to be unreachable.
+
+        This code below works because never type can be coerced to any other type.
+    */
+    let x = match "123".parse::<i32>() {
+        Ok(x) => x,
+        Err(_) => panic!(),
+    };
+
+    // let x: ! = return;
+    let x: String = return;
+    let counter = 0;
+
+    /*
+        In this case never type is treated as equivalentto unit.
+        All standard documentation considered the break, continue, return,
+        to result in a never type.
+    */
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break;
+        }
+    };
 }
