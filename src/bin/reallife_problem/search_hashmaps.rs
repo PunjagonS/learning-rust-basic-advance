514ac7649a6826d92c83a8c8d72380d7c69f25e4
PUNJAGONS <punjagons@gmail.com>
2024-09-09 11:49:16 +0700

    add more desciptive comment.

--- src/bin/reallife_problem/search_hashmaps.rs
+++ src/bin/reallife_problem/search_hashmaps.rs
@@ -14,10 +14,16 @@ use std::collections::HashMap;
 fn word_groupings(words_list: Vec<String>) -> Vec<Vec<String>> {
     let mut word_hash = HashMap::new();
 
+    // define vec! 26 lengths (0-25) with init 0 value ( vec![0,0,0,0, ... ] )
     let mut char_freq = vec![0; 26];
 
     for current_word in words_list {
         for c in current_word.to_lowercase().chars() {
+            /*
+                Calculate each char ascii to map position align with
+                vec![26 index] define that `a` is index 0, `b` is index 1, ...
+                and calculated index is incremented by 1 (frequency count).
+            */
             char_freq[(c as u32 - 'a' as u32) as usize] += 1;
         }
 
@@ -37,9 +43,10 @@ fn word_groupings(words_list: Vec<String>) -> Vec<Vec<String>> {
         println!("key # {:?} value # {:?}", key, value);
     }
 
+    // old fasion to extract the value from hashmap by ignoring key `_`.
     // word_hash.into_iter().map(|(_, v)| v).collect()
 
-    // using `into_values()` as compiler recommended
+    // using `into_values()` as compiler recommended to directly extract value only.
     word_hash.into_values().collect()
 }
 
@@ -57,6 +64,7 @@ fn main() {
     let grouping = word_groupings(words);
 
     let input_word = String::from("teh");
+    println!("Input word : {:?}", input_word);
     for i in grouping.into_iter() {
         if i.contains(&input_word) {
             println!("The group of word is {:?}", i);
