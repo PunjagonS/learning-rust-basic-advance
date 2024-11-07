b1f10bc8fd41833db33ee0d9475d16a6b7a18915
PUNJAGONS <punjagons@gmail.com>
2024-11-07 09:49:55 +0700

    extract closure to fn handler.

--- src/bin/axum/hello_router.rs
+++ src/bin/axum/hello_router.rs
@@ -2,14 +2,19 @@
 
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let routes_hello = Router::new().route(
         "/hello",
-        get(|| async { Html("Hello, <strong>Axum!</strong>") }),
+        // get(|| async { Html("Hello, <strong>Axum!</strong>") }),
+        get(handler_hello),
     );
 
     // region: --- Start Server
@@ -19,3 +24,10 @@ async fn main() {
     axum::serve(listener, routes_hello).await.unwrap();
     // endregion: --- Start Server
 }
+
+// region: --- Handler Hello
+async fn handler_hello() -> impl IntoResponse {
+    println!("--> {:<12} - handler_hello", "HANDLER");
+    Html("Hello, <strong>Axum!</strong>")
+}
+// endregion: --- Handler Hello
